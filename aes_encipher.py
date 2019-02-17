5a79e4bbdf6568bf054f40a74ce5ece50bad6b08
藤岡 祐 (FUJIOKA Yuu) <fujioka@cyphertec.co.jp>
2019-02-17 11:21:29 +0900

    cleanup code

--- aes_encipher.py
+++ aes_encipher.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import argparse
 import struct
 import json
@@ -7,30 +8,43 @@ from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES
 from AesEncipher import AesEncipher
 
+
 MAX_BUFFER_SIZE = 4096
 KEY_LENGTH = 32
 
 
-if __name__ == '__main__':
-    
+def set_argument_parser():
+
     parser = argparse.ArgumentParser(description='AES(CTR Mode) Encipher Program')
     parser.add_argument('tool_mode', help='chose e (encrypt) or d (decrypt)')
     parser.add_argument('input_file', help='file to en/de-crypt')
     parser.add_argument('--nonce', help='nonce to decrypt', type = str, default = "")
     parser.add_argument('--key', help='key to decrypt', type = str, default = "")
-    args = parser.parse_args()
+    return parser.parse_args()
 
-    if args.tool_mode != 'e' and args.tool_mode != 'd' :
+
+def check_args(args):
+
+    if args.tool_mode != 'e' and args.tool_mode != 'd':
         print("error: args1: undefined parameter")
         sys.exit()
+    if not os.path.isfile(args.input_file):
+        print("error: args2: file not exists")
+        sys.exit()
+    if args.tool_mode == 'd' and args.nonce == "":
+        print("error: --nonce is null: need nonce to use decrypt mode")
+        sys.exit()
+    if args.tool_mode == 'd' and args.key == "":
+        print("error: --key is null: need key to use decrypt mode")
+        sys.exit()
+
+
+if __name__ == '__main__':
+    
+    args = set_argument_parser()
+    check_args(args)
 
     encrypt_mode = True if args.tool_mode == 'e' else False
-    if not encrypt_mode and args.nonce == "":
-        print("error: args3 is null: need nonce to use decrypt mode")
-        sys.exit()
-    if not encrypt_mode and args.key == "":
-        print("error: args4 is null: need key to use decrypt mode")
-        sys.exit()
 
     key = get_random_bytes(KEY_LENGTH) if args.key == "" else b64decode(args.key)
     aes_encipher = AesEncipher(AES.MODE_CTR, key)
